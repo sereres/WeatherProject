d7a9075d9c115857a520bc15e3481f401a179558
sereres <sereres@gmail.com>
2019-07-14 17:32:42 -0500

    five tests

--- WeatherFlights/TripTests.py
+++ WeatherFlights/TripTests.py
@@ -10,10 +10,12 @@ class TripTest(unittest.TestCase):
         self.originLatt = 41.8781
         self.originLong = 87.6298
         self.destinationName = "Atlanta"
-        #self.destinationLatt = 33.7490
-        #self.destinationLong = 84.3880
+        self.destinationLatt = 33.7490
+        self.destinationLong = 84.3880
         self.aTrip = WeatherFlights.Trips.Trip(self.originName, self.destinationName)
         self.aCity = WeatherFlights.Trips.City(self.originName, self.originLatt, self.originLong)
+        self.aCity2 = WeatherFlights.Trips.City(self.destinationName, self.destinationLatt, self.destinationLong)		
+        self.aCityTrip = WeatherFlights.Trips.Trip(self.aCity, self.aCity2)
 		
     def testOrigin(self):
         self.assertEqual(self.aTrip.origin, self.originName, "Trips origin set")	
@@ -23,6 +25,10 @@ class TripTest(unittest.TestCase):
 		
     def testCity(self):
         self.assertEqual(self.aCity.lattitude, self.originLatt, "city lattitude set")
+		
+    def testCityTrip(self):
+        self.assertEqual(self.aCityTrip.origin.name, self.aCity.name, "test trips between cities")
+	
 
 		
 if __name__ == "__main__":
