b4af69fda88cd84f172afb0027f4b88aee3b373c
sereres <sereres@gmail.com>
2019-07-14 16:11:27 -0500

    add city as object

--- WeatherFlights/TripTests.py
+++ WeatherFlights/TripTests.py
@@ -5,19 +5,25 @@ class TripTest(unittest.TestCase):
     def testTests(self):
         pass
 		
-    def testTrip(self):
-        self.TripOrigin = "Chicago"
-        self.TripDestination = "Atlanta"
-        self.assertNotEqual(self.TripOrigin, self.TripDestination, 
-                            "Trip must have some length")
-		
     def setUp(self):
         self.originName = "Chicago"
+        self.originLatt = 41.8781
+        self.originLong = 87.6298
         self.destinationName = "Atlanta"
+        #self.destinationLatt = 33.7490
+        #self.destinationLong = 84.3880
         self.aTrip = WeatherFlights.Trips.Trip(self.originName, self.destinationName)
+        self.aCity = WeatherFlights.Trips.City(self.originName, self.originLatt, self.originLong)
 		
     def testOrigin(self):
         self.assertEqual(self.aTrip.origin, self.originName, "Trips origin set")	
-	
+		
+    def testPath(self):
+        self.assertNotEqual(self.aTrip.origin, self.aTrip.destination, "Trip must have some length")
+		
+    def testCity(self):
+        self.assertEqual(self.aCity.lattitude, self.originLatt, "city lattitude set")
+
+		
 if __name__ == "__main__":
     unittest.main()
--- WeatherFlights/Trips.py
+++ WeatherFlights/Trips.py
@@ -4,3 +4,11 @@ class Trip(object):
         self.origin = origin
         self.destination = destination
 	    
+class City(object):
+
+    def __init__(self, name, lattitude, longitude):
+        self.name = name
+        self.lattitude = lattitude
+        self.longitude = longitude
+		
+	
