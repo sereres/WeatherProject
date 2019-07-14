09c4578903de00f95ec2c3be2959b3534571a457
sereres <sereres@gmail.com>
2019-07-14 13:59:35 -0500

    Intitial Structure

---
+++ WeatherFlights/TripTests.py
@@ -0,0 +1,15 @@
+import unittest
+#import WeatherFlights.Trip
+
+class Tests(unittest.TestCase):
+    def TestTests(self):
+        pass
+		
+#    def TestTrip(self):
+#        self.origin = "Chicago"
+#        self.destination = "Atlanta"
+		
+#        self.Trip = WeatherFlights.Trip.Path(self.origin, self.destination)
+	
+if __name__ == "__main__":
+    unittest.main()
---
+++ runAllTests.bat
@@ -0,0 +1,2 @@
+set PYTHONPATH=.
+python WeatherFlights/TripTests.py
